452d760b744bfdcd0f008b3ef0dc076c82a5dd59
Vladimir <sivakvladimir0@gmail.com>
2025-07-12 02:22:59 +0300

    3

--- src/main/java/ru/vladimir/tgBot/entity/OrderProduct.java
+++ src/main/java/ru/vladimir/tgBot/entity/OrderProduct.java
@@ -1,63 +1,50 @@
-//package ru.vladimir.tgBot.entity;
-//import jakarta.persistence.*;
-//
-//@Entity
-//public class OrderProduct {
-//    @Id
-//    @GeneratedValue
-//    private Long id;
-//
-//    @ManyToOne
-//    private ClientOrder order;
-//
-//    @ManyToOne
-//    private Product product;
-//
-//    @Column(nullable = false)
-//    private Integer countProduct;
 package ru.vladimir.tgBot.entity;
-
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 
 @Entity
-@Table(
-        name = "order_product",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"order_id", "product_id"})
-        }
-)
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class OrderProduct {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "order_id", nullable = false)
+    @ManyToOne
     private ClientOrder order;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "product_id", nullable = false)
+    @ManyToOne
     private Product product;
 
     @Column(nullable = false)
     private Integer countProduct;
 
-    public OrderProduct() {}
+    public Long getId() {
+        return id;
+    }
 
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public ClientOrder getOrder() { return order; }
-    public void setOrder(ClientOrder order) { this.order = order; }
+    public ClientOrder getOrder() {
+        return order;
+    }
 
-    public Product getProduct() { return product; }
-    public void setProduct(Product product) { this.product = product; }
+    public void setOrder(ClientOrder order) {
+        this.order = order;
+    }
 
-    public Integer getCountProduct() { return countProduct; }
-    public void setCountProduct(Integer countProduct) { this.countProduct = countProduct; }
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getCountProduct() {
+        return countProduct;
+    }
+
+    public void setCountProduct(Integer countProduct) {
+        this.countProduct = countProduct;
+    }
 }
